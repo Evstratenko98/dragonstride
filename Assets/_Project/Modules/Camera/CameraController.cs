86b7a4f276c4515cea5e4730acfd0c97bcea9cde
Stanislav Evstratenko <evstratenko.sv98@gmail.com>
2026-01-27 13:37:57 +0300

    fix camera

--- Assets/_Project/Modules/Camera/CameraController.cs
+++ Assets/_Project/Modules/Camera/CameraController.cs
@@ -134,8 +134,8 @@ public class CameraController : ITickable, IPostInitializable, IDisposable
     {
         var cellSize = _config.CELL_SIZE;
         _minX = 0f;
-        _minZ = 0f;
+        _minZ = -(_config.FIELD_HEIGHT / 2);
         _maxX = Mathf.Max(0f, (_config.FIELD_WIDTH - 1) * cellSize);
-        _maxZ = Mathf.Max(0f, (_config.FIELD_HEIGHT - 1) * cellSize);
+        _maxZ = Mathf.Max(0f, (_config.FIELD_HEIGHT - 1) * (cellSize / 2));
     }
 }
